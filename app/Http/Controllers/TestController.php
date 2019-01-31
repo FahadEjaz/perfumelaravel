5fd3f70bc6868df13fa917a6a38befd49e27eff4
root <root@mage.local>
2019-01-31 13:18:15 +0500

    Model dump incomplete

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
+use App\Product;
 use Maatwebsite\Excel\Facades\Excel;
 
 class TestController extends Controller
@@ -20,10 +21,23 @@ class TestController extends Controller
         if (!$process->isSuccessful()) {                        
             if (strpos($output, 'differ') !== false) {
                 $data = Excel::toCollection(null, 'pricelist.xlsx', 'local');
-                for($i=0;$i<4;$i++){
+                for($i=0;$i<5;$i++){
                     unset($data[0][$i]);
                 }
-                print_r($data);
+                print_r($data);//exit;
+                $product = new Product;
+                foreach ($data[0] as $item) {
+                    $product->sku = $item[0];
+                    $product->description = $item[1];
+                    $product->upc = $item[2];
+                    $product->price = $item[3];
+                    $product->availability = $item[4];
+                    $product->total = $item[6];
+                    $product->type = $item[7];
+                    $product->brand = $item[8];
+                    $product->save();
+                }
+                exit;
             }
         }
         if($output == ""){
